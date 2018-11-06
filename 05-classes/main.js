010d4c8d419d222571e57c8f2a58a4aa495433f4
wanessamarcondes <wanessamarcondes@gmail.com>
2018-11-06 15:48:50 -0200

    exercicios mover resolvidos

--- 05-classes/main.js
+++ 05-classes/main.js
@@ -45,8 +45,44 @@ class Div extends Ponto {
         this.node.style.width = this.largura + "px"
         this.node.style.height = this.altura + "px"
         this.node.style.backgroundColor = this.cor
-        this.node.style.top = this.x + "px"
-        this.node.style.left = this.y + "px"
+        this.node.style.top = this.y + "px"
+        this.node.style.left = this.x + "px"
         body.appendChild(this.node)
     }
+
+    mover_na_tela(direcao) {
+        const incremento = 10
+        if (direcao === "up") {
+            this.mover_verticalmente(-incremento)
+            this.node.style.top = this.y + "px"
+        } else if (direcao === "down") {
+            this.mover_verticalmente(incremento)
+            this.node.style.top = this.y + "px"
+        } else if (direcao === "left") {
+            this.mover_horizontalmente(-incremento)
+            this.node.style.left = this.x + "px"
+        } else if (direcao === "right") {
+            this.mover_horizontalmente(incremento)
+            this.node.style.left = this.x + "px"
+        }
+    }
 }
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowUp") {
+        div1.mover_na_tela("up")
+    } else if (event.key === "ArrowDown") {
+        div1.mover_na_tela("down")
+    } else if (event.key === "ArrowLeft") {
+        div1.mover_na_tela("left")
+    } else if (event.key === "ArrowRight") {
+        div1.mover_na_tela("right")
+    }
+})
+
+const div1 = new Div("wanessa", "pink", 0, 25, 250, 250)
+const div2 = new Div("azul", "aqua", 300, 25, 250, 250)
+const div3 = new Div("verdinha", "green", 600, 25, 250, 250)
+div1.desenhar()
+div2.desenhar()
+div3.desenhar()
